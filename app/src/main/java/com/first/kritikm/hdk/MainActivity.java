f1d2d33b48566ecf0687ed46deac822fba462f79
Harshil Gupta <Harshil Gupta>
2016-10-23 03:45:01 +0530

    Added empty gallery thingy

--- app/src/main/java/com/first/kritikm/hdk/MainActivity.java
+++ app/src/main/java/com/first/kritikm/hdk/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
@@ -18,13 +19,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.first.kritikm.hdk.Databases.Photos;
 import com.first.kritikm.hdk.Databases.PhotosTags;
 import com.github.clans.fab.FloatingActionMenu;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import junit.framework.*;
 
@@ -42,8 +48,11 @@ public class MainActivity extends AppCompatActivity {
     private final int RESULT_GALLERY = 2;
     private GridView imageGrid;
     private ArrayList<Uri> uris;
+
+    ImageView i;
     Bitmap pathBitmap = null;
     ImageAdapter imageAdapter;
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +60,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         isStoragePermissionGranted();
         imageAdapter = new ImageAdapter(this, R.layout.grid_row, new ArrayList<Uri>());
-        menu = (FloatingActionMenu)findViewById(R.id.fabMenu);
+        menu = (FloatingActionMenu) findViewById(R.id.fabMenu);
         menu.setClosedOnTouchOutside(true);
         imageGrid = (GridView) findViewById(R.id.imageGrid);
 
         imageGrid.setAdapter(imageAdapter);
+        if (imageGrid.getChildCount() == 0) {
+            i = (ImageView) findViewById(R.id.img1);
+            i.setVisibility(View.VISIBLE);
+        }
+
+
+
     }
+
 
 //    private ArrayList<Bitmap> getSavedImages()
 //    {
@@ -71,39 +88,37 @@ public class MainActivity extends AppCompatActivity {
 //        return bitmaps;
 //    }
 
-    public void getImageFromCamera(View view)
-    {
+    public void getImageFromCamera(View view) {
+        menu.close(true);
         getImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(getImage, RESULT_CAMERA);
     }
-    public void getImageFromGallery(View view)
-    {
+
+    public void getImageFromGallery(View view) {
+        menu.close(true);
         getImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(getImage, RESULT_GALLERY);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(resultCode == RESULT_OK)
-        {
-            if(requestCode == RESULT_CAMERA || requestCode == RESULT_GALLERY)
-            {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RESULT_CAMERA || requestCode == RESULT_GALLERY) {
                 if (resultCode == RESULT_OK) {
                     if (requestCode == RESULT_CAMERA || requestCode == RESULT_GALLERY) {
                         Uri path = null;
                         if (requestCode == RESULT_GALLERY) {
                             path = handleGallerySelect(data);
-                        }
-                        else if (requestCode == RESULT_CAMERA) {
+                        } else if (requestCode == RESULT_CAMERA) {
                             path = handleCameraSelect(data);
                         }
-                        Log.d(Commons.TAG,path.toString());
+                        Log.d(Commons.TAG, path.toString());
                         imageAdapter.add(path);
                         imageAdapter.notifyDataSetChanged();
+                        i.setVisibility(View.INVISIBLE);
 
-                       // bitmaps.add(pathToBitmap(path.toString()));
-                       // imageGrid.setAdapter(new ImageAdapter(getBaseContext(), bitmaps));
+                        // bitmaps.add(pathToBitmap(path.toString()));
+                        // imageGrid.setAdapter(new ImageAdapter(getBaseContext(), bitmaps));
 
                     }
 
@@ -123,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         return ImageHelper.saveToExternalStorage(bm);
     }
 
-    public  boolean isStoragePermissionGranted() {
+    public boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED) {
@@ -131,13 +146,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             } else {
 
-                Log.d(Commons.TAG,"Permission is revoked");
+                Log.d(Commons.TAG, "Permission is revoked");
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 return false;
             }
-        }
-        else { //permission is automatically granted on sdk<23 upon installation
-            Log.d(Commons.TAG,"Permission is granted");
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.d(Commons.TAG, "Permission is granted");
 
             return true;
         }
@@ -146,10 +160,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
-            Log.d(Commons.TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(Commons.TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
             //resume tasks needing this permission
         }
     }
+
 
 }
